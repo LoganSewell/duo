304ea2b796ea20d9f5fd2630be782e4eecbcb4c0
Logan Sewell <47334784+LoganSewell@users.noreply.github.com>
2024-09-30 13:45:51 -0500

    Update database.js

---
+++ scripts/database.js
@@ -0,0 +1,30 @@
+export { database }
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ]
+}
